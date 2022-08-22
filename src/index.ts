8ee76fd2bb2c505906696fbf20d07ed3cef33734
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-22 12:04:18 -0300

    feat: config and implement api routes

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ dotenv.config();
 
 import express from "express";
 import { PORT } from "./config/env";
+import apiRouter from "./api";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server init on port ${PORT}`);
