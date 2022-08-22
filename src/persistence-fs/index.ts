b4bb284619822639358be1d3d4ce73c599d1c77f
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-22 12:39:38 -0300

    feat: add cart persistence instance

--- src/persistence-fs/index.ts
+++ src/persistence-fs/index.ts
@@ -1,4 +1,5 @@
-import type { Product, ProductFS } from "../interfaces";
+import type { Product, ProductFS, Cart, CartFS } from "../interfaces";
 import { Persistence } from "./base";
 
 export const productsFs = new Persistence<Product, ProductFS>("products");
+export const cartFs = new Persistence<Cart, CartFS>("cart");
