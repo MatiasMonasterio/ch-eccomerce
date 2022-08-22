c96fe271890231cd3988314aedc59bc714f44710
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-22 12:50:38 -0300

    feat: add 404 route not found

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
+app.all("*", (req, res) => {
+  res.status(404).json({ error: -1, description: `Can't find ${req.originalUrl} on this server!` });
+});
+
 app.listen(PORT, () => {
   console.log(`Server init on port ${PORT}`);
 });
