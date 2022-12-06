2491ab90d45ef484fcf493a0f624c2f338138325
MatiasMonasterio <matias_monasterio@outlook.com>
2022-12-06 20:15:44 -0300

    feat: implement is authorized middleware

--- src/api/routes/products/products.routes.ts
+++ src/api/routes/products/products.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
 import { productController } from "../../controllers";
-import { isAdmin } from "../../middlewares";
+import { isAdmin, isAuthorized } from "../../middlewares";
 
 const productRouter = Router();
 
-productRouter.get("/", productController.getAll);
+productRouter.get("/", isAuthorized, productController.getAll);
 productRouter.get("/:productId", productController.getOne);
 productRouter.post("/", isAdmin, productController.createOne);
 productRouter.put("/:productId", isAdmin, productController.updateOne);
